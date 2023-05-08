0e9e2facc155ba1e29182d6c61908cd155573dae
Steven Salto <ssalto2702@gmail.com>
2023-05-08 00:41:41 -0400

    removed colors dep

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const { Client } = require('pg');
 const { getDatabaseURI } = require('./config')
 
-require('colors')
-
 const db = new Client({ connectionString:getDatabaseURI() })
 
 db.connect((err) => {
 if (err) {
-    console.error('Connection error'.red, err.stack);
+    console.error('Connection error', err.stack);
     } else {
-    console.log('Successfully connected to postgres db!'.blue);
+    console.log('Successfully connected to postgres db!');
     }
 });
 
